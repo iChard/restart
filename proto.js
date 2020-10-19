cdfc15bc0b41698b877841251c87c614786f8a60
刘佩玉 <rulecoder@gmail.com>
2020-10-19 20:52:36 +0800

    1

--- proto.js
+++ proto.js
@@ -40,7 +40,7 @@ console.log(Foo.prototype.isPrototypeOf(foo))
 console.log(a.isPrototypeOf(b))
 console.log(Object.getOwnPropertyDescriptors(foo).__proto__)
 
-// 实现__proto__1122
+// 实现__proto__11
 Object.defineProperty(Object.prototype, '__proto__', {
     get: function() {
         return Object.getPrototypeOf(this)
